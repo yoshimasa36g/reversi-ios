1485021277d33db1a9454cacc6367a1c816f4e72
yoshimasa36g <ashihana785@gmail.com>
2020-05-20 16:00:12 +0900

    Adjust group

---
+++ Reversi/Controller/GameControllerInput.swift
@@ -0,0 +1,13 @@
+//
+//  GameControllerInput.swift
+//  Reversi
+//
+//  Created by yoshimasa36g on 2020/05/20.
+//  Copyright © 2020 yoshimasa36g. All rights reserved.
+//
+
+import Foundation
+
+protocol GameControllerInput {
+
+}
